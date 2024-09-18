5067c8e4176544aaba680e52fdbe81f70a0bfc8e
Cecilia <cefr@itu.dk>
2024-09-18 11:12:43 +0200

    not working code

--- rustengine/src/main.rs
+++ rustengine/src/main.rs
@@ -1,10 +1,15 @@
 use std::fs; 
 use std::io;
 
-fn count_words(String content) -> usize{
+fn count_words(content: String) -> usize{
     let mut count: usize = 0; 
     let content_splitted = content.split_whitespace();
 
+    while!content_splitted.isEmpty(){
+        count += 1;
+    }
+
+    count
 }
 
 fn print_type_of<T>(_: &T) {
@@ -24,15 +29,15 @@ fn main() {
     let content = fs::read_to_string(file_path)
     .expect("Should have been able to read the file");
 
-    let word_count = content.split_whitespace();
+    let word_count = count_words(content);
 
     //println!("The type of word_count is {}", type.of(&word_count));
 
-    print_type_of(&content);
-    print_type_of(&word_count );
+    //print_type_of(&content);
+    //print_type_of(&word_count );
 
 
-    //println!("The file holds {} words.", word_count);
+    println!("The file holds {} words.", word_count);
 }
 
 // cargo run 
