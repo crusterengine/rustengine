449da96e882db8d13b2a25747904716dc4ed73e3
Cecilia <cefr@itu.dk>
2024-09-19 09:36:55 +0200

    Updated so logs is stored in a seperate folder and added these folders to gitignore

--- benchmarking/benchmarking_c.sh
+++ benchmarking/benchmarking_c.sh
@@ -4,6 +4,13 @@
 #which time
 #type -a time
 
+
+#This creates a folder for the logs if it does not already exist
+mkdir -p "log_folder_c"
+
+#This captures the path to where the log files should be located
+log_folder="/Users/ejer/Desktop/rustengine/benchmarking/log_folder_c"
+
 #This creates a 'variable' that contains the path to the file I want to use in my program
 input_file="../data/the-champion.txt"
 
@@ -11,7 +18,7 @@ input_file="../data/the-champion.txt"
 cd ../cengine
 
 #Benchmarks the
-gtime -v ./scanner <<< "$input_file"
+gtime -v ./scanner <<< "$input_file" 2> "$log_folder/time_output.txt"
 
 #To redirect the log
 #time -o timing.log ls -l
--- benchmarking/benchmarking_rust.sh
+++ benchmarking/benchmarking_rust.sh
@@ -4,7 +4,13 @@
 #which time
 #type -a time
 
-#This creates a 'variable' that contains the path to the file I want to use in my program
+#This creates a folder for the logs if it does not already exist
+mkdir -p "log_folder_rust"
+
+#This captures the path to where the log files should be located
+log_folder="/Users/ejer/Desktop/rustengine/benchmarking/log_folder_rust"
+
+#This creates a 'variable' that contains the path to the file you want to use in the benchmarking 
 input_file="../../../data/the-champion.txt"
 
 #Go into the directory of the file you want to time
@@ -12,9 +18,8 @@ cd ../rustengine_1point1/target/debug
 
 #/usr/bin/time -v ./scanner <<< "$input_file"
 
-#Benchmarks the
-gtime -v ./rustengine_1point1 "$input_file"
-
+#Benchmarks the rust program based on the input file hardcoded further up
+gtime -v ./rustengine_1point1 "$input_file" 2> "$log_folder/time_output.txt"
 
 #To redirect the log
 #time -o timing.log ls -l
