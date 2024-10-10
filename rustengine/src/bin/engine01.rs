44cccb7e96d4a9020e8c67c91cfa65ae689f0003
Cecilia <cefr@itu.dk>
2024-10-10 14:05:13 +0200

    made another rust counting method but commented it out

--- rustengine/src/bin/engine01.rs
+++ rustengine/src/bin/engine01.rs
@@ -2,18 +2,6 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 
-// fn file_processing(file_path: &str, word_count: &mut usize){
-
-//     let file = File::open(file_path).expect("Should have been able to read the file"); //Type = std::fs::File
-//     let reader = io::BufReader::new(file); //Type = std::io::buffered::bufreader::BufReader<std::fs::File>
-
-//     for line in reader.lines() {
-//         let line = line.expect("Expected to find a line");
-//         *word_count += line.split_whitespace().count();
-//     }
-
-// }
-
 fn file_processing(file_path: &str, word_count: &mut usize){
 
     let file = File::open(file_path).expect("Should have been able to read the file"); //Type = std::fs::File
@@ -21,13 +9,25 @@ fn file_processing(file_path: &str, word_count: &mut usize){
 
     for line in reader.lines() {
         let line = line.expect("Expected to find a line");
-
-        for _word in line.split_whitespace() {
-            *word_count += 1;
-        }
-
+        *word_count += line.split_whitespace().count();
     }
+
 }
+
+// fn file_processing(file_path: &str, word_count: &mut usize){
+
+//     let file = File::open(file_path).expect("Should have been able to read the file"); //Type = std::fs::File
+//     let reader = io::BufReader::new(file); //Type = std::io::buffered::bufreader::BufReader<std::fs::File>
+
+//     for line in reader.lines() {
+//         let line = line.expect("Expected to find a line");
+
+//         for _word in line.split_whitespace() {
+//             *word_count += 1;
+//         }
+
+//     }
+// }
 
 fn main() {
 
